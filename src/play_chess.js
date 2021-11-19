72e8bf3ee293980d6b296b8f3fa556aea83a4d7b
Jeff Kayombo <jeffkayombo16@gmail.com>
2021-11-20 01:14:15 +0200

    Add play chess file

---
+++ src/play_chess.js
@@ -0,0 +1,43 @@
+export default class PlayChess {
+  constructor() {
+    this.gameKey = '8b1WjNYLhiocCWi4SZXe';
+    this.baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+  }
+
+  fetchScores =  async () => {
+    return await (await fetch(`${this.baseUrl}games/${this.gameKey}/scores/`)).json();
+  }
+  
+  displayScores() {
+    const scoreList = document.querySelector('.score-list');
+    this.fetchScores().then((res) => {
+      if (res.result && typeof res.result === 'object') {
+        const scores = res.result;
+        if (scores.length > 0) {
+          scoreList.innerHTML = '';
+          scores.forEach((score) => (scoreList.innerHTML += `<li class="items">${score.user} : ${score.score}</li>`));
+        } else {
+          scoreList.innerHTML = 'Oops! This scoreboard looks empty. How about you begin by adding something?';
+        }
+      }
+    })
+  }
+
+  addScore = async (user,score) => {
+   let response = 'error';
+    if (user && score) {
+      response = await (await fetch(`${this.baseUrl}games/${this.gameKey}/scores`, {
+        method: 'POST',
+        body: JSON.stringify({
+          user,
+          score,
+        }),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      })).json();
+    }
+    return response;
+  }
+
+}
